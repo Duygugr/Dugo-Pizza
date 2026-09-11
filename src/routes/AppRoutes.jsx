f4742d93b7a8aa85295489fdcd12d45c77bcf2ae
Duygu Görmez <duygugr01@gmail.com>
2026-09-11 21:31:43 +0300

    vite güncellendi.

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Route , Routes} from "react-router-dom"
+import { HashRouter, Route , Routes} from "react-router-dom"
 import Home from '../pages/Home'
 import Hakkımızda from '../pages/Hakkımızda'
 import Ürünler from "../pages/Ürünler"
